457a044e79cf4516f96f8a85c13418561e4346d8
Frank Ittermann <frank.ittermann@goflink.com>
2025-12-10 21:17:13 +0100

    chore(debiancgo): update Go version to 1.25.5
    Update the Golang version from 1.25.4 to 1.25.5.
    
    - Update Golang version to 1.25.5

--- pkg/golang/debiancgo/docker_metadata_gen.go
+++ pkg/golang/debiancgo/docker_metadata_gen.go
@@ -5,14 +5,14 @@ package debiancgo
 // Default Dockerfile (Dockerfilego)
 const (
 	// ImageVersion is the golang version extracted from the Dockerfile
-	ImageVersion = "1.25.4"
+	ImageVersion = "1.25.5"
 
 	// DockerfileChecksum is the checksum of the Dockerfile content
-	DockerfileChecksum = "afdf56054901a8dd63a947be24ee6d57925ff7cde5c6dbdc0013b27af3b952b7"
+	DockerfileChecksum = "016a5fd7f041991f3e64cf35e5cac534c3c456bba2a1fcd5fd45b1864fc4674d"
 )
 
 // DockerfileContent contains the embedded Dockerfile content
-var DockerfileContent = `FROM golang:1.25.4
+var DockerfileContent = `FROM golang:1.25.5
 
 RUN apt-get update && \
     apt-get install -y clang build-essential \
