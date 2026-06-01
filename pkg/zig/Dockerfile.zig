74dfb9f9478b6adf64efc30913d0e4de193e4ea7
fr12k <frank.ittermann@yahoo.de>
2026-06-01 20:06:50 +0200

    fix(zig): use proper arch

--- pkg/zig/Dockerfile.zig
+++ pkg/zig/Dockerfile.zig
@@ -2,9 +2,13 @@ FROM --platform=$TARGETPLATFORM alpine:3.23
 ARG TARGETPLATFORM
 ARG BUILDPLATFORM
 ARG ZIG_VERSION=0.17.0-dev.263+0add2dfc4
-ARG ZIG_ARCH=x86_64  # Define the architecture
 
 RUN apk add --no-cache curl xz && \
+    case "$TARGETPLATFORM" in \
+        linux/amd64)  ZIG_ARCH=x86_64  ;; \
+        linux/arm64)  ZIG_ARCH=aarch64 ;; \
+        *) echo "Unsupported platform: $TARGETPLATFORM" && exit 1 ;; \
+    esac && \
     curl -L https://ziglang.org/builds/zig-${ZIG_ARCH}-linux-${ZIG_VERSION}.tar.xz \
     | tar -xJ -C /usr/local && \
     ln -s /usr/local/zig-${ZIG_ARCH}-linux-${ZIG_VERSION}/zig /usr/local/bin/zig
