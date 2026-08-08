02258c62e07b5e23a1a9751133c50a8144f911c9
Frank <126682769+fr12k@users.noreply.github.com>
2026-08-08 18:07:53 +0200

    chore(zig): update to zig version 1609+11e2bb391 (#528)
    Co-authored-by: fr12k <frank.ittermann@yahoo.de>

--- pkg/zig/Dockerfile.zig
+++ pkg/zig/Dockerfile.zig
@@ -1,5 +1,5 @@
 FROM alpine:3.24
-ARG ZIG_VERSION=0.17.0-dev.1422+e863bf3be
+ARG ZIG_VERSION=0.17.0-dev.1609+11e2bb391
 
 RUN apk add --no-cache curl xz && \
     ZIG_ARCH=$(uname -m | sed 's/arm64/aarch64/' | sed 's/amd64/x86_64/') && \
