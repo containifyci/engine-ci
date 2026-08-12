2b6f98db98886b82329c0d6eae77beadf28c0a5c
Frank <126682769+fr12k@users.noreply.github.com>
2026-08-12 08:41:31 +0200

    chore: fix invalid go file

--- pkg/goreleaser/docker_metadata_gen.go
+++ pkg/goreleaser/docker_metadata_gen.go
@@ -10,6 +10,7 @@ const (
 	// DockerfileChecksum is the checksum of the Dockerfile content
 
 	DockerfileChecksum = "5fc517a43b1ab2bef5d342f5adbe1a5fa62b8aa7145758bb5b69d7c5e0caf75e"
+)
 
 // DockerfileContent contains the embedded Dockerfile content
 var DockerfileContent = `FROM goreleaser/goreleaser:v2.17.1
